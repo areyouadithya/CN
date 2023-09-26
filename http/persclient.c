97fed28694435ec6d7a321babbb9fef0b0595bff
Adithya R U <112653455+areyouadithya@users.noreply.github.com>
2023-09-26 11:01:02 +0530

    Update persclient.c

--- http/persclient.c
+++ http/persclient.c
@@ -3,49 +3,63 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <sys/socket.h>
+
+#define SERVER_IP "127.0.0.1"
+#define PORT 8080
+#define MAX_REQUESTS 3
+
+void send_http_request(int sockfd, int request_number) {
+    char request[] = "GET / HTTP/1.1\r\nHost: " SERVER_IP "\r\nConnection: keep-alive\r\n\r\n";
+
+    printf("Sending Request #%d:\n%s", request_number, request);
+    send(sockfd, request, strlen(request), 0);
+}
 
 int main() {
-    int client_socket;
+    int sockfd;
     struct sockaddr_in server_addr;
 
-    // Create socket
-    client_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (client_socket == -1) {
-        perror("Error creating client socket");
-        exit(1);
+    // Create a socket
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
     }
 
-    // Configure server address
-    memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1"); // Server IP address
-    server_addr.sin_port = htons(8080); // Server port
+    server_addr.sin_port = htons(PORT);
+
+    // Convert the IP address from string to binary
+    if (inet_pton(AF_INET, SERVER_IP, &server_addr.sin_addr) <= 0) {
+        perror("Invalid address/ Address not supported");
+        exit(EXIT_FAILURE);
+    }
 
     // Connect to the server
-    if (connect(client_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
-        perror("Error connecting to server");
-        close(client_socket);
-        exit(1);
+    if (connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Connection failed");
+        exit(EXIT_FAILURE);
     }
 
-    printf("Connected to server\n");
+    for (int i = 1; i <= MAX_REQUESTS; i++) {
+        send_http_request(sockfd, i);
 
-    // Send an HTTP request (simplified for demonstration)
-    const char *request = "GET / HTTP/1.1\r\nHost: localhost\r\n\r\n";
-    send(client_socket, request, strlen(request), 0);
-    printf("Sent HTTP request\n");
+        char response[4096];
+        ssize_t bytes_received;
 
-    // Receive and print the server's response
-    char buffer[1024];
-    int bytes_received = recv(client_socket, buffer, sizeof(buffer), 0);
-    if (bytes_received > 0) {
-        buffer[bytes_received] = '\0';
-        printf("Server response:\n%s\n", buffer);
+        // Receive and print the server's response
+        bytes_received = recv(sockfd, response, sizeof(response) - 1, 0);
+
+        if (bytes_received > 0) {
+            response[bytes_received] = '\0';
+            printf("\nResponse #%d received:\n%s\n", i, response);
+        }
+
+        // Connection remains open for the next request
     }
 
-    // Close the client socket
-    close(client_socket);
+    // Close the socket after all requests
+    close(sockfd);
 
     return 0;
 }
-
